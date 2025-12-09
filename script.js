7eee827fae15d3cbce17e08b4e08b32fb5c983d6
JuliaBarten <58068451+JuliaBarten@users.noreply.github.com>
2025-12-09 16:17:10 +0100

    login functie user aanmaken

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-const items = document.querySelectorAll('.item');
+const items = document.querySelectorAll(".item");
 
 
-//https://dev.to/okonkwomandy/the-easiest-way-to-build-the-pinterest-layout-without-using-a-framework-3i0g
+//https://dev.to/okonkwomandy/the-easiest-way-to-build-the-pinterest-layout-without-using-a-framework-3i0g0
 
 // The number of columns is assigned to this variable
 const numCols = 2;
 
 // The masonry container is assigned to this variable
-const masonryCon = document.querySelector('.boxes-con');
+const masonryCon = document.querySelector(".boxes-con");
 
-// The masonry container elements is assigned to this variable 
-const list = document.querySelectorAll('.boxes');
-
+// The masonry container elements is assigned to this variable
+const list = document.querySelectorAll(".boxes");
 
 // The number of columns will be used to create HTML div elements that are appended to the masonry container
 for (var i = 1; i < numCols + 1; i++) {
-  let elem = document.createElement('div');
-  elem.classList.add('sub');
-  elem.classList.add('box' + i);
+  let elem = document.createElement("div");
+  elem.classList.add("sub");
+  elem.classList.add("box" + i);
   masonryCon.append(elem);
 }
 
@@ -43,7 +42,7 @@ for (var j = 0; j < timesBy; j++) {
 }
 
 // The div elements that created based on the number of columns are assigned to variable
-subs = document.querySelectorAll('.sub');
+subs = document.querySelectorAll(".sub");
 
 // The elements are assigned to their position based on the number of columns
 for (var b = 0; b < themainarray.length; b++) {
@@ -51,11 +50,14 @@ for (var b = 0; b < themainarray.length; b++) {
 }
 
 // for responsiveness, get the the width of the device
-function pageWidth() {  
-return window.innerWidth != null? window.innerWidth :
-document.documentElement &&
-document.documentElement.clientWidth ?
-document.documentElement.clientWidth :
-document.body != null ? document.body.clientWidth : null; }
+function pageWidth() {
+  return window.innerWidth != null
+    ? window.innerWidth
+    : document.documentElement && document.documentElement.clientWidth
+      ? document.documentElement.clientWidth
+      : document.body != null
+        ? document.body.clientWidth
+        : null;
+}
 
 width = pageWidth();
