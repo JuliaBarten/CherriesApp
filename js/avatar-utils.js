c8d4cd00cdf01c739d20c248ceb615078c3c56f9
JuliaBarten <58068451+JuliaBarten@users.noreply.github.com>
2026-01-17 01:44:42 +0100

    Reorder premade avatars: adjust the order of avatar images for better selection variety.

--- js/avatar-utils.js
+++ js/avatar-utils.js
@@ -1,10 +1,11 @@
 export const PREMADE_AVATARS = [
-  "images/avatar/premade/pm_1.png",
   "images/avatar/premade/pm_2.png",
   "images/avatar/premade/pm_3.png",
-  "images/avatar/premade/pm_4.png",
   "images/avatar/premade/pm_5.png",
-  "images/avatar/premade/pm_6.png"
+  "images/avatar/premade/pm_1.png",
+  "images/avatar/premade/pm_6.png",
+  "images/avatar/premade/pm_7.png",
+  "images/avatar/premade/pm_4.png",
 ];
 
 export function getRandomAvatar() {
